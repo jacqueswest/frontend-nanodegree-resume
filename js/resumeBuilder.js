e613698c2877a577a86e0022f8974ead0fa9820b
jacqueswest <jacqueswest@gmail.com>
2015-12-10 12:32:22 +0200

    course progres changes

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,17 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+//$("#main").append(["Jacques van der Westhuizen"]);
+
+//var awesomethoughts = "My name is JAcques and I am the real superman";
+
+//console.log(awesomethoughts);
+
+//var funthoughts = awesomethoughts.replace("superman", "globalman");
+
+//$("#main").append(funthoughts);
+
+var formattedname = HTMLheaderName.replace("%data%", "Jacques van der Westhuizen");
+var formattedrole = HTMLheaderRole.replace("%data%", "Python Developer");
+
+$("#header").prepend(formattedname);
+$("#header").append(formattedrole);
+
+
