85914c2aad8df286d3b992b2f054aa954af4220a
jacqueswest <jacqueswest@gmail.com>
2015-12-15 15:56:46 +0200

    course progres changes

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -14,4 +14,23 @@ var formattedrole = HTMLheaderRole.replace("%data%", "Python Developer");
 $("#header").prepend(formattedname);
 $("#header").append(formattedrole);
 
+var bio = {"name": "Jacques",
+           "role": "Automation Engineer",
+		   "contact_info": "jacqueswest@gmail.com",
+		   "pic_url": "",
+		   "welcome_message": "Welcome to me CV",
+		   "skills": "Python Dev"
+		  };
+		  
+$("#header").append(bio.welcome_message);
+$("#header").append(bio.role);
+$("#header").append(bio.skills);
+$("#header").append(bio.contact_info);
 
+//document.getElementById("#header").style.backgroundColor = "black"; 
+
+/* if (bio["skills"].lenght > 0;){
+	$("#header").append(HTMLskillsStart);
+	
+	var formattedSkill = HTMLskills.replace("%data%", bio.skills[0]);
+} */
